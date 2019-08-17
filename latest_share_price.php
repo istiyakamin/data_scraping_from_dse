83c2cb5c43adb9e5dc38db8b75226937271a606b
istiyakaminsanto <istiyakaminsanto@gmail.com>
2019-08-17 16:32:15 +0600

    some file added

--- latest_share_price.php
+++ latest_share_price.php
@@ -1,15 +1,10 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);  
+//error_reporting(E_ALL ^ E_NOTICE);  
 //header('Content-Type: application/json');
 
-$method = $_SERVER['REQUEST_METHOD'];
 
-//GET Request
-switch ($method) {
-    case 'GET': // read data
-    getOperation();
-    break;
-}
+getOperation();
+
 
 //GET Function
 function getOperation(){
@@ -44,44 +39,36 @@ function getOperation(){
   foreach($elements as $e){
   
     $len = $e->childNodes->length;
-    //echo $len;
 
-        
-        echo $trade_date = date('Y-m-d');
-        echo $trading_code = $e->childNodes[1]->nodeValue;
-        echo $ltp = (int)$e->childNodes[2]->nodeValue;
-        echo $high = (int)$e->childNodes[3]->nodeValue;
-        echo $low = (int)$e->childNodes[4]->nodeValue;
-        echo $closep = (int)$e->childNodes[5]->nodeValue;
-        echo $ycp = (int)$e->childNodes[6]->nodeValue;
-        echo $change = (int)$e->childNodes[7]->nodeValue;
-        echo $trade = (int)$e->childNodes[8]->nodeValue;
-        echo $value = (int)$e->childNodes[9]->nodeValue;
-        echo $volume = (int)$e->childNodes[10]->nodeValue;
-        echo "</br>";
-
-     
-        $count++;
+    echo $trade_date = date('Y-m-d');
+    echo $trading_code = $e->childNodes[1]->nodeValue;
+    echo $ltp = str_replace(',', '', $e->childNodes[2]->nodeValue);
+    echo $high = str_replace(',', '', $e->childNodes[3]->nodeValue);
+    echo $low = str_replace(',', '', $e->childNodes[4]->nodeValue);
+    echo $closep = str_replace(',', '', $e->childNodes[5]->nodeValue);
+    echo $ycp = str_replace(',', '', $e->childNodes[6]->nodeValue);
+    echo $change = str_replace(',', '', $e->childNodes[7]->nodeValue);
+    echo $trade = str_replace(',', '', $e->childNodes[8]->nodeValue);
+    echo $value = str_replace(',', '', $e->childNodes[9]->nodeValue);
+    echo $volume = str_replace(',', '', $e->childNodes[10]->nodeValue);
+    echo "</br>";
+    $count++;
        
 
-        if($count > 1){
-          $insert_query = "INSERT INTO ta.dse(date, tradeing_code, ltp, high, low, openp, closep, ycp, trade, value, volume) 
-          VALUES ('{$trade_date}', '{$trading_code}', '{$ltp}', '{$high}', '{$low}', '{$closep}', '{$ycp}', '{$change}', '{$trade}', '{$value}', '{$volume}');
-          ";
+    if($count > 1){
+        $insert_query = "INSERT INTO ta.dse(date, tradeing_code, ltp, high, low, openp, closep, ycp, trade, value, volume) 
+        VALUES ('{$trade_date}', '{$trading_code}', '{$ltp}', '{$high}', '{$low}', '{$closep}', '{$ycp}', '{$change}', '{$trade}', '{$value}', '{$volume}');
+        ";
         $success_result = pg_query($conn, $insert_query);
         //echo $insert_query;
         //die;
-        
+    
         if (!$success_result) {
             echo "Error in query execution occured. : ".$insert_query."\n";
             die;
         }
     }
-
-   
-
   }
  
-  }
+}
    
-?>
